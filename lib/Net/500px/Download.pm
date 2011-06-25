88808ed5f5ae3a84db556631dd744b1f321985db
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-06-25 15:27:24 +0900

    initial commit

---
+++ lib/Net/500px/Download.pm
@@ -0,0 +1,32 @@
+package Net::500px::Download;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::500px::Download -
+
+=head1 SYNOPSIS
+
+  use Net::500px::Download;
+
+=head1 DESCRIPTION
+
+Net::500px::Download is
+
+=head1 AUTHOR
+
+kuniyoshi kouji E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
